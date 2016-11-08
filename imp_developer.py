1c67235aed0d3fcdf5b662b65febe8675bbecf10
Pavel Petroshenko <pavel@electricimp.com>
2016-11-07 16:27:20 -0800

    Extended the template to support more error messages.

--- imp_developer.py
+++ imp_developer.py
@@ -1291,7 +1291,7 @@ class LogManager:
         if log["type"] in ["server.error", "agent.error"]:
             # agent/device runtime errors
             preprocessor = self.env.code_processor
-            pattern = re.compile(r"ERROR:\s*(?:at|from)\s*(\S+)\s*.*:(\d+)")
+            pattern = re.compile(r"ERROR:\s*(?:at|from|in)\s*(\S+)\s*.*:(\d+)")
             match = pattern.match(log["message"])
             log_debug(("[RECOGNIZED]  " if match else "[UNRECOGNIZED]") +
                       "  [ ] Original runtime error: " + log["message"])
